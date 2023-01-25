105cdbf8430ea26ec40c1654d0186fb2fed35ec2
Rishabh Mehta <rishabh.mh1@gmail.com>
2023-01-25 19:13:30 +0530

    neural network

--- car.js
+++ car.js
@@ -1,98 +1,36 @@
-class Car {
-  constructor(x, y, width, height) {
-    this.x = x
-    this.y = y
-    this.width = width
-    this.height = height
+const carCanvas = document.getElementById('carCanvas')
+carCanvas.width = 200
+const networkCanvas = document.getElementById('networkCanvas')
+networkCanvas.width = 300
 
-    this.speed = 0
-    this.acceleration = 0.2
-    this.maxSpeed = 3
-    this.friction = 0.05
-    this.angle = 0
-    this.damaged = false
+const carCtx = carCanvas.getContext('2d')
+const networkCtx = networkCanvas.getContext('2d')
 
-    this.sensor = new Sensor(this)
-    this.controls = new Controls()
+const road = new Road(carCanvas.width / 2, carCanvas.width * 0.9)
+const car = new Car(road.getLaneCenter(1), 100, 30, 50, 'AI')
+const traffic = [new Car(road.getLaneCenter(1), -100, 30, 50, 'DUMMY', 2)]
+
+animate()
+
+function animate() {
+  for (let i = 0; i < traffic.length; i++) {
+    traffic[i].update(road.borders, [])
   }
+  car.update(road.borders, traffic)
 
-  update(roadBorders) {
-    if (!this.damaged) {
-      this.$move()
-      this.polygon = this.#createPolygon()
-      this.damaged = this.#assessDamage(roadBorders)
-    }
-    this.sensor.update(roadBorders)
+  carCanvas.height = window.innerHeight
+  networkCanvas.height = window.innerHeight
+
+  carCtx.save()
+  carCtx.translate(0, -car.y + carCanvas.height * 0.7)
+
+  road.draw(carCtx)
+  for (let i = 0; i < traffic.length; i++) {
+    traffic[i].draw(carCtx, 'red')
   }
+  car.draw(carCtx, 'blue')
 
-  #assessDamage(roadBorders) {
-    for (let i = 0; i < roadBorders.length; i++) {
-      if (polysIntersecet(this.polygon, roadBorders[i])) return true
-    }
-    return false
-  }
+  carCtx.restore()
 
-  #createPolygon() {
-    const points = []
-    const rad = Math.hypot(this.width, this.height) / 2
-    const alpha = Math.atan2(this.width, this.height)
-    points.push({
-      x: this.x - Math.sin(this.angle - alpha) * rad,
-      y: this.y - Math.cos(this.angle - alpha) * rad,
-    })
-    points.push({
-      x: this.x - Math.sin(this.angle + alpha) * rad,
-      y: this.y - Math.cos(this.angle + alpha) * rad,
-    })
-    points.push({
-      x: this.x - Math.sin(Math.PI + this.angle - alpha) * rad,
-      y: this.y - Math.cos(Math.PI + this.angle - alpha) * rad,
-    })
-    points.push({
-      x: this.x - Math.sin(Math.PI + this.angle + alpha) * rad,
-      y: this.y - Math.cos(Math.PI + this.angle + alpha) * rad,
-    })
-
-    return points
-  }
-
-  $move() {
-    if (this.controls.forward) this.speed += this.acceleration
-
-    if (this.controls.reverse) this.speed -= this.acceleration
-
-    if (this.speed > this.maxSpeed) this.speed = this.maxSpeed
-
-    if (this.speed < -this.maxSpeed) this.speed = -this.maxSpeed / 2
-
-    if (this.speed > 0) this.speed -= this.friction
-
-    if (this.speed < 0) this.speed += this.friction
-
-    if (Math.abs(this.speed) < this.friction) this.speed = 0
-
-    if (this.speed != 0) {
-      const flip = this.speed > 0 ? 1 : -1
-      if (this.controls.left) this.angle += 0.03 * flip
-
-      if (this.controls.right) this.angle -= 0.03 * flip
-    }
-
-    this.x -= Math.sin(this.angle) * this.speed
-
-    this.y -= Math.cos(this.angle) * this.speed
-  }
-
-  draw(ctx) {
-    if (this.damaged) ctx.fillStyle = 'gray'
-    else ctx.fillStyle = 'black'
-    ctx.beginPath()
-    ctx.moveTo(this.polygon[0].x, this.polygon[0].y)
-    for (let i = 1; i < this.polygon.length; i++) {
-      ctx.lineTo(this.polygon[i].x, this.polygon[i].y)
-    }
-    ctx.fill()
-
-    this.sensor.draw(ctx)
-  }
+  requestAnimationFrame(animate)
 }
